f12e42069f21f7e339e2f421fd06b712b2b5a7ac
Ivan Grigorev <iv.st.grigorev@gmail.com>
2021-07-21 13:54:41 +0300

    Module_5

---
+++ Block_1-Python_Core/Module_5/lesson_m5-1.py
@@ -0,0 +1,470 @@
+# for i in range(30):
+#     s = "int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}".format(i)
+#     print(s)
+#
+# width = 5
+# for num in range(5):
+#     print('{:^10} {:^10} {:^10}'.format(num, num**2, num**3))
+#
+#
+# def sanitize(data):
+#     while True:
+#         start_index = data.find("[")
+#         end_index = data.find("]")
+#         if start_index == -1:
+#             break
+#         data = data[:start_index] + data[end_index + 1:]
+#     return data
+#
+#
+# print(sanitize(string))
+#
+# def count_digits(string):
+#     count = 0
+#     for el in string:
+#         if el.isdigit():
+#             count += 1
+#     return count
+#
+#
+# print(count_digits(string))
+#
+#
+# def count_numbers(string):
+#     count = 0
+#     position = 0
+#     while position < len(string):
+#         if string[position].isdigit():
+#             while string[position].isdigit():
+#                 print(position, string[position])
+#                 position += 1
+#             count += 1
+#         else:
+#             position += 1
+#     return count
+#
+# print(count_numbers(string))
+#
+# numbers = ["123", "456", "100", "10", "13", "abc", "23a"]
+#
+#
+# def sanitize(data):
+#     result =[]
+#     for el in data:
+#         if el.isdigit():
+#             result.append(el)
+#     return result
+#
+#
+# def transform_to_int(data):
+#     result =[]
+#     for el in data:
+#         result.append(int(el))
+#     return result
+#
+#
+# sanitize_numbers = sanitize(numbers)
+# sanitize_numbers = transform_to_int(sanitize_numbers)
+#
+# sorted_sanitize_numbers = sorted(sanitize_numbers, reverse=True)
+#
+# print(max(sanitize_numbers))
+# print(min(sanitize_numbers))
+# print(sum(sanitize_numbers)/len(sanitize_numbers))
+#
+# """
+# Есть список телефонов. Убрать все ошибочные строки и привести всех к одинаковому виду. ["380669640547",
+# "0637306465", "380961935171", "632643973", "508325200", "000000000", "48730283918", "986223575", "375297947963"]
+# """
+#
+# phone_storage = [" 380669640547 ", " 0637306465", "380961935171", "632643973 ", "508325200 ", "000000000",
+#                  "48730283918",
+#                  "986223575", "375297947963", "38050964+05+47", "38050xxxxxxx", "+38(050)123-45-67",
+#                  "38(050)123 45 67"]
+#
+# """
+# +380501234567  + 38 code country 050 code operator tel: 1234567
+# """
+#
+# codes_operators = {"039", "050", "063", "066", "067", "068", "073", "091"}
+#
+#
+# def _is_valid_phone(phone):
+#     if len(phone) == 12:
+#         if phone[:2] == '38':
+#             if phone[2:5] in codes_operators:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return False
+#     if len(phone) == 10:
+#         if phone[:3] in codes_operators:
+#             return True
+#         else:
+#             return False
+#     return False
+#
+#
+# def is_valid_phone(phone):
+#     def is_valid_operator(phone):
+#         if phone[:3] in codes_operators:
+#             return True
+#         return False
+#
+#     if phone.isdigit():
+#         if len(phone) == 12:
+#             if phone[:2] == '38':
+#                 return is_valid_operator(phone[2:])
+#         if len(phone) == 10:
+#             return is_valid_operator(phone)
+#     return False
+
+#
+# def sanitize_phone(phone):
+#     new_phone = (phone.strip()
+#                  .removeprefix("+")
+#                  .replace("(", "")
+#                  .replace(")", "")
+#                  .replace(" ", "")
+#                  .replace("-", ""))
+#     return new_phone
+#
+#
+# # for phone in phone_storage:
+# #     phone = sanitize_phone(phone)
+# #     is_valid = is_valid_phone(phone)
+# #     if is_valid:
+# #         print("Телефон {:>12} валиден".format(phone))
+# #     else:
+# #         print("Телефон {:>12} не валиден".format(phone))
+#
+# def pretty_table():
+#     print("|{:^14}|{:^12}|".format("Телефон", "Результат"))
+#     print("|{:^14}|{:^12}|".format("-" * 14, "-" * 12))
+#     for phone in phone_storage:
+#         phone = sanitize_phone(phone)
+#         is_valid = is_valid_phone(phone)
+#         if is_valid:
+#             print("|{:>14}|{:^12}|".format(phone, "валиден"))
+#         else:
+#             print("|{:>14}|{:^12}|".format(phone, "не валиден"))
+#
+#
+# if __name__ == "__main__":
+#     pretty_table()
+# #
+# url = "https://www.google.com/search?q=Cat+and+dog&ie=utf-8&oe=utf-8&aq=t"
+#
+# # Парсим строку запрос и превращаем в словарь
+# _, query_search = url.split("?")  # ["https://www.google.com/search", "q=Cat+and+dog&ie=utf-8&oe=utf-8&aq=t"]
+# print(query_search)
+# list_parameters = query_search.split("&")
+# print(list_parameters)
+#
+# object_search = {}
+# for el in list_parameters:
+#     key, value = el.split("=")
+#     object_search.update({key: value.replace("+", " ")})
+#
+# print(object_search)
+#
+# # Формируем строку запрос
+# url_string = ''
+# for key, value in object_search.items():
+#     url_string = url_string + "=".join([key, value.replace(" ", "+")])
+# print(url_string)
+#
+# """
+# Есть список 2 списка телефонов: первый - список всех регистраций, второй - тех, кто подключились к игре.
+# Определить список телефонов тех, кто зарегистрировался, но не начал работу на сайте.
+# ["380669640547", "0637306465",
+# "380961935171", "632643973", "508325200", "000000000", "48730283918", "986223575", "375297947963"]
+# ["380669640547", "0637306465" "632643973", "508325200", "48730283918", "986223575"]
+# """
+# from myphone import is_valid_phone
+#
+# registration = []
+#
+# for phone in ["380669640547", "0637306465", "380961935171", "632643973", "508325200", "000000000", "48730283918",
+#               "986223575", "375297947963"]:
+#     if is_valid_phone(phone):
+#         registration.append(phone)
+#
+# connected = []
+#
+# for phone in ["380669640547", "0637306465" "632643973", "508325200", "48730283918", "986223575"]:
+#     if is_valid_phone(phone):
+#         connected.append(phone)
+#
+# print(registration)
+# print(connected)
+#
+# result = list(set(registration) & set(connected))
+# print(result)
+#
+# url_rozetka = "https://rozetka.com.ua/mobile-phones/c80003/kolichestvo-osnovnih-kamer=3630921;producer=xiaomi;23777=6" \
+#               "-6-5;38435=677049/ "
+#
+# query_url_rozetka = "kolichestvo-osnovnih-kamer=3630921;producer=xiaomi;23777=6-6-5;38435=677049"
+#
+# query_rozetka = query_url_rozetka.split(";")
+#
+# print(query_rozetka)
+#
+# object_rozetka = {}
+# for el in query_rozetka:
+#     key, value = el.split("=")
+#     object_rozetka.update({key: value})
+#
+# print(object_rozetka)
+#
+#
+#
+# def format_ingredients(items):
+#     items = ', '.join(items)
+#     recipe = items[::-1].replace(","[::-1], " и"[::-1], 1)[::-1]
+#     return recipe
+#
+#
+# print(format_ingredients(["onion", "cucumber", "apple", "holly shit", "2 pieces of something"]))
+#
+# def lookup_key(data, value):
+#     result = []
+#     for i, j in data.items():
+#         if j == value:
+#             result.append(i)
+#     return result
+#
+#
+# print(lookup_key({"onion": 45}, 45))
+#
+
+# def split_list(grade):
+#     list_1 = []
+#     list_2 = []
+#     result = (list_1, list_2)
+#     if len(grade) == 0:
+#         return result
+#     average = sum(grade) / len(grade)
+#     for i in grade:
+#         if i <= average:
+#             list_1.append(i)
+#         elif i > average:
+#             list_2.append(i)
+#     print(f 'grade: {grade} | result: {result}')
+#     return result
+#
+#
+# split_list([11, 4, 3, 1, 6, 9, 5])
+
+
+# points = {
+#     (0, 1): 2,
+#     (0, 2): 3.8,
+#     (0, 3): 2.7,
+#     (1, 2): 2.5,
+#     (1, 3): 4.1,
+#     (2, 3): 3.9
+# }
+#
+#
+# def calculate_distance(coordinates):
+#     key_points = []
+#     distance = []
+#     total_distance = sum(distance)
+#     if len(coordinates) == 0 and len(coordinates) == 1:
+#         return 0
+#     for i in coordinates:
+#         key_points.append(i)
+#         if len(key_points) == 2:
+#             for key, value in points.items():
+#                 if key == key_points:
+#                     distance.append(value)
+#     return total_distance
+#
+#
+# print(calculate_distance([0, 1, 3, 2, 0, 2]))
+
+
+
+
+# points = {
+#     (0, 1): 2,
+#     (0, 2): 3.8,
+#     (0, 3): 2.7,
+#     (1, 2): 2.5,
+#     (1, 3): 4.1,
+#     (2, 3): 3.9,
+# }
+#
+#
+# def calculate_distance(coordinates):
+#     distance = 0
+#     step = []
+#     if len(coordinates) > 1:
+#         for i in coordinates[:-1]:
+#             step.append(sorted(coordinates[0:2]))
+#             coordinates = coordinates[1:]
+#     for i in step:
+#         i = tuple(i)
+#         if tuple(i) in points.keys():
+#             distance += points.get(i)
+#     return distance
+#
+#
+# print(calculate_distance([0, 1, 3, 2, 0, 2]))
+#
+
+
+#
+# def game(terra, power):
+#     for i in terra:
+#         for e in i:
+#             if e <= power:
+#                 power += e
+#             if e > power:
+#                 break
+#     return power
+
+
+# def is_valid_pin_codes(pin_codes):
+#     valid_pin_codes = []
+#     for i in pin_codes:
+#         if len(i) == 4:
+#             if i.isdigit():
+#                 valid_pin_codes.append(i)
+#         for j in valid_pin_codes:
+#             if j not in pin_codes:
+#                 return True
+#             else:
+#                 return False
+#
+
+# def is_valid_pin_codes(pin_codes):
+#     codes = []
+#     if pin_codes == []:
+#         return False
+#     for i in pin_codes:
+#         print(i)
+#         if i.isdigit() and i == str(i) and i not in codes and i != "" and len(i) == 4:
+#             codes.append(i)
+#         else:
+#             return False
+#     return True
+
+#
+# print(is_valid_pin_codes(['1101', '9034', '0011']))
+# print(is_valid_pin_codes([]))
+
+# from random import randint
+#
+#
+# def get_random_password():
+#     random_choice = []
+#     key = [randint(40, 126) for i in range(0, 8)]
+#     for j in key:
+#         j = chr(j)
+#         random_choice.append(j)
+#         continue
+#     random_password = "".join(random_choice)
+#     return random_password
+#
+#
+# get_random_password()
+
+
+#
+# def is_valid_password(password):
+#     number = False
+#     high_symbol = False
+#     lower_symbol = False
+#
+#     for i in password:
+#         if i in '1234567890':
+#             number = True
+#         elif i == i.upper() and i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#             high_symbol = True
+#         elif i == i.lower() and i in 'qwertyuiopasdfghjklzxcvbnm':
+#             lower_symbol = True
+#     if number and high_symbol and lower_symbol and len(password) == 8:
+#         return True
+#     return False
+#
+#
+# print(is_valid_password('123Fa111'))
+#
+#
+# from random import randint
+#
+#
+# def get_random_password():
+#     result = ""
+#     count = 0
+#     while count < 8:
+#         random_symbol = chr(randint(40, 126))
+#         result = result + random_symbol
+#         count = count + 1
+#     return result
+#
+#
+# def is_valid_password(password):
+#     has_upper = False
+#     has_lower = False
+#     has_num = False
+#     for ch in password:
+#         if ch >= "A" and ch <= "Z":
+#             has_upper = True
+#         elif ch >= "a" and ch <= "z":
+#             has_lower = True
+#         elif ch >= "0" and ch <= "9":
+#             has_num = True
+#     if len(password) == 8 and has_upper and has_lower and has_num:
+#         return True
+#     return False
+#
+#
+# def get_password():
+#     while True:
+#         unique_password = get_random_password()
+#         if is_valid_password(unique_password):
+#             return unique_password
+#         else:
+#             continue
+#
+#
+# print(get_password())
+#
+#
+# """
+# Напишите функцию parse_folder, она принимает единственный параметр path,
+# который является объектом Path. Функция должна просканировать директорию path
+# и вернуть кортеж из двух списков. Первый это список файлов внутри директории,
+# второй список директорий.
+#
+# def parse_folder(path):
+#     files = []
+#     folders = []
+#     for i in path.iterdir():
+#         if i.is_dir():
+#             folders += [i.name]
+#         else:
+#             files += [i.name]
+#     return files, folders
+# """
+#
+# print(__doc__)
+
+
+import sys
+
+
+def parse_args():
+    result = ""
+    new_list = []
+    for i in sys.argv:
+        new_list.append(i)
+
+    result = ' '.join(new_list[1:])
+    return result
+
---
+++ Block_1-Python_Core/Module_5/lesson_m5-2.py
@@ -0,0 +1,470 @@
+# import re
+#
+# string = "Нильс Бор родился в семье профессора физиологии Копенгагенского университета Христиана Бора (1858—1911), " \
+#          "дважды становившегося кандидатом на Нобелевскую премию по физиологии и медицине[10], и Эллен Адлер (" \
+#          "1860—1930), дочери влиятельного и весьма состоятельного еврейского банкира и парламентария-либерала Давида " \
+#          "Баруха Адлера (дат. David Baruch Adler; 1826—1878) и Дженни Рафаэль (1830—1902) из британской еврейской " \
+#          "банкирской династии Raphael Raphael & sons[en][11]. Родители Бора поженились в 1881 году."
+#
+# pattern = r'\d+'
+# result = re.search(pattern, string)
+# # print(result.span())
+# # print(result.group())
+# # print(result.string)
+#
+# pattern = r'\d+'
+# result = len(re.findall(pattern, string))
+# print(result)
+#
+#
+# def count_numbers(string):
+#     return len(re.findall(r'\d', string))
+#
+# result = count_numbers(string)
+# print(result)
+#
+# result = re.findall(r'[0-9]{4}', string)
+# print(result)
+#
+# pattern = r'[А-Я]'
+# new_pattern = re.compile(pattern)
+# result = new_pattern.findall(string)
+# print(result)
+
+
+# import re
+#
+# google_url = "q=Cat+and+dog&ie=utf-8&oe=utf-8&aq=t"
+# rozetka_url = "kolichestvo-osnovnih-kamer=3630921;producer=xiaomi;23777=6-6-5;38435=677049"
+#
+#
+# def get_object_parameters(query, pattern="&|;", key_split="="):
+#     object_search = {}
+#     for el in re.split(pattern, query):
+#         key, value = el.split(key_split)
+#         object_search.update({key: value.replace("+", " ")})
+#     return object_search
+#
+#
+# print(get_object_parameters(google_url))
+# print(get_object_parameters(rozetka_url))
+
+
+# import re
+#
+# string = "Исключить из этой [строки группы] символов, [расположенные между] скобками [, ]."
+#
+# lang = "The best language Java"
+# pattern = "Java"
+#
+# result = re.sub(pattern, "Python", lang)
+# print(result)
+#
+# pattern = r"\[.*?\]"
+# result = re.sub(pattern, "", string)
+# print(result)
+
+
+# import re
+#
+# string = "Нильс Бор родился в семье профессора физиологии Копенгагенского университета Христиана Бора (1858—1911), " \
+#          "дважды становившегося кандидатом на Нобелевскую премию по физиологии и медицине[10], и Эллен Адлер (" \
+#          "1860—1930), дочери влиятельного и весьма состоятельного еврейского банкира и парламентария-либерала Давида " \
+#          "Баруха Адлера (дат. David Baruch Adler; 1826—1878) и Дженни Рафаэль (1830—1902) из британской еврейской " \
+#          "банкирской династии Raphael Raphael & sons[en][11]. Родители Бора поженились в 1881 году."
+#
+# # print(re.findall(r'[^\w]', string))
+#
+# # print(re.findall(r'^\w+', string))
+# # print(re.findall(r'(\w+)\.?$', string))
+#
+# # pattern = r"[А-Яа-я]{3}\b"
+# # print(re.findall(pattern, string))
+#
+# # pattern = r"\b\d{3}"
+# # print(re.findall(pattern, string))
+#
+# # print(re.findall(r"\d{4}—\d{4}", string))
+# # print(re.findall(r"\d{4}—(\d{4})", string))
+# # print(re.findall(r"(\d{4})—\d{4}", string))
+# # print(re.findall(r"(\d{4})—(\d{4})", string))
+#
+# result_list = []
+# iterator = re.finditer(r"\d{4}—\d{4}", string)
+# for match in iterator:
+#     result_list.append(match.group())
+# # print(result_list)
+#
+# text = "Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com " \
+#        "abc111@test.com.net "
+#
+# print(re.findall(r"@\w+\.\w{2,5}", text))
+# print(re.findall(r"@\w+\.(\w{2,5})", text))
+
+
+# map = {ord('з'): 'z', ord('ю'): 'uu'}
+# translated = 'p.зюзюыызю'.translate(map)
+# print(translated)  # zu
+#
+# symbols = "0123456789ABCDEF"
+# code = [
+#         '0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111',
+#         '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111'
+#         ]
+#
+# MAP = {}
+#
+# print(zip(symbols, code))
+#
+# for s, c in zip(symbols, code):
+#     MAP[ord(s)] = c
+#     MAP[ord(s.lower())] = c
+#
+# result = "34 DF 56 AC".translate(MAP)
+# print(result)
+
+#
+# def real_len(text):
+#     text_list = list(text)
+#     result = []
+#     print(text_list)
+#     for i in text_list:
+#         if i != '\n' and i != '\f' and i != '\r' and i != '\t' and i != '\v':
+#             result.append(i)
+#     print(result)
+#     print(len(result))
+#     return len(result)
+
+#
+# real_len('gdgd\n\t')
+#
+
+#
+# articles_dict = [
+#     {
+#         "title": "Minim voluptate eu aliqua duis pariatur cupidatat voluptate.",
+#         "author": "Jhon Stark",
+#         "year": 2019,
+#     },
+#     {
+#         "title": "Dolore Lorem aliquip est labore elit labore ex consequat ad occaecat duis.",
+#         "author": "Artur Clark",
+#         "year": 2020,
+#     },
+#     {
+#         "title": "Aliqua minim amet ut pariatur et occaecat esse qui commodo ut duis sunt elit.",
+#         "author": "Silver Name",
+#         "year": 2021,
+#     },
+#     {
+#         "title": "Irure reprehenderit aliquip officia quis occaecat aute mollit laborum ullamco laboris Lorem commodo.",
+#         "author": "Golden Gun",
+#         "year": 2021,
+#     },
+# ]
+#
+#
+# def find_articles(key, letter_case=False):
+#     coincidence = False
+#     new_list = []
+#     if letter_case:
+#         for i in articles_dict:
+#             for j in i.values():
+#                 try:
+#                     if j.find(key) != -1:
+#                         coincidence = True
+#                 except:
+#                     continue
+#             if coincidence:
+#                 new_list.append(i)
+#             coincidence = False
+#     else:
+#         for i in articles_dict:
+#             for j in i.values():
+#                 try:
+#                     key.upper()
+#                     j.upper()
+#                     if j.find(key) != -1:
+#                         coincidence = True
+#                 except:
+#                     continue
+#             if coincidence:
+#                 new_list.append(i)
+#             coincidence = False
+#
+#     return new_list
+
+
+# def sanitize_phone_number(phone):
+#     new_phone = (phone.strip()
+#                  .removeprefix("+")
+#                  .replace("(", "")
+#                  .replace(")", "")
+#                  .replace(" ", "")
+#                  .replace("-", ""))
+#     print(new_phone)
+#     return new_phone
+#
+#
+# sanitize_phone_number("    +38(050)123-32-34")
+
+#
+# def is_check_name(fullname, first_name):
+#     if first_name in fullname:
+#         return True
+#     for i in fullname:
+#         if i.isupper():
+#             return False
+#     else:
+#         return True
+#
+#
+# def sanitize_phone_number(phone):
+#     new_phone = (
+#             phone.strip()
+#             .removeprefix("+")
+#             .replace("(", "")
+#             .replace(")", "")
+#             .replace("-", "")
+#             .replace(" ", "")
+#             .replace("+", "")
+#         )
+#     return new_phone
+#
+#
+# def get_phone_numbers_for_countries(list_phones):
+#     ua = []
+#     jp = []
+#     tw = []
+#     sg = []
+#     result = dict(UA=ua, JP=jp, TW=tw, SG=sg)
+#     list_phones1 = str(list_phones)
+#     list_phones2 = sanitize_phone_number(list_phones1)
+#     if list_phones2.startswith('380'):
+#         ua.append(list_phones2)
+#     if list_phones2.startswith('81'):
+#         jp.append(list_phones2)
+#     if list_phones2.startswith('886'):
+#         tw.append(list_phones2)
+#     if list_phones2.startswith('65'):
+#         sg.append(list_phones2)
+#     if not list_phones2.startswith('380')\
+#             and not list_phones2.startswith('81')\
+#             and not list_phones2.startswith('886')\
+#             and not list_phones2.startswith('65'):
+#         ua.append(list_phones2)
+#     return print(result)
+#
+#
+# get_phone_numbers_for_countries(["+380508509978", "+380509002254", "+886509002254", "+444509002254"])
+
+
+# def sanitize_phone_number(phone):
+#     new_phone = (
+#         phone.strip()
+#         .removeprefix("+")
+#         .replace("(", "")
+#         .replace(")", "")
+#         .replace("-", "")
+#         .replace(" ", "")
+#     )
+#     return new_phone
+#
+#
+# def get_phone_numbers_for_countries(list_phones):
+#     phone_dict = {'JP':[], 'SG':[], 'TW':[], "UA":[]}
+#     for i in list_phones:
+#         sannumber = sanitize_phone_number(i)
+#         if sannumber.startswith('380'):
+#             phone_dict['UA'].append(sannumber)
+#         elif sannumber.startswith('81'):
+#             phone_dict['JP'].append(sannumber)
+#         elif sannumber.startswith("65"):
+#             phone_dict['SG'].append(sannumber)
+#         elif sannumber.startswith('886'):
+#             phone_dict['TW'].append(sannumber)
+#         else:
+#             phone_dict['UA'].append(sannumber)
+#     return phone_dict
+#
+#
+# print(get_phone_numbers_for_countries(["+380508509978", "+380509002254", "+886509002254", "+444509002254"]))
+
+#
+# def is_spam_words(text, spam_words, space_around=False) -> bool:
+#     spam_words = str()
+#     for spam_words in text:
+#         space_terms = text.index(spam_words) == 0 or spam_words.find(" ") \
+#                        and spam_words.rfind(".") or spam_words.rfind(" ")
+#         if spam_words in text and spam_words != space_terms and space_around == False:
+#             return True
+#     for spam_words in text:
+#         space_terms = text.index(spam_words) == 0 or spam_words.find(" ") \
+#                        and spam_words.rfind(".") or spam_words.rfind(" ")
+#         if spam_words in text and spam_words == space_terms and space_around == True:
+#             return False
+#     else:
+#         return False
+#
+#
+# print(is_spam_words("Молох", ["лох"]))
+# print(is_spam_words("Молох", ["лох"], True))
+
+
+#
+# CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+# TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l",
+#                "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ts", "ch",
+#                "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+#
+# TRANS = {}
+# for i, j in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+#     TRANS[ord(i)] = j
+#     TRANS[ord(i.upper())] = j.upper()
+#
+#
+# def translate(name):
+#     return name.translate(TRANS)
+#
+#
+# print(translate("Дмитрий Коробов"))
+# print(translate("Александр Иванович"))
+
+#
+# grade = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
+#
+#
+# def formatted_grades(students):
+#     counter = 0
+#     for key, value in students.items():
+#         for i, j in grade.items():
+#             if value == i:
+#                 counter += 1
+#                 print("{:>4}|{:<10}|{:^5}|{:^5}".format(counter, key, i, j))
+#     return
+#
+#
+# formatted_grades({"Nick": "A", "Olga": "B", "Mike": "FX", "Anna": "C"})
+
+#
+# grade = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
+#
+#
+# def formatted_grades(students):
+#     count = 0
+#     new_list = []
+#     for i, j in students.items():
+#         for x, z in grade.items():
+#             if j == x:
+#                 count += 1
+#                 spisok = '{:>4}|{:<10}|{:^5}|{:^5}'.format(count, i, x, z)
+#                 new_list.append(spisok)
+#     return new_list
+#
+#
+# students = {"Nick": "A", "Olga": "B", "Mike": "FX", "Anna": "C"}
+#
+# print(formatted_grades(students))
+
+#
+# def formatted_numbers():
+#     num_list = []
+#     table_title = "|{:^10}|{:^10}|{:^10}|".format('decimal', 'hex', 'binary')
+#     num_list.append(table_title)
+#     for i in range(16):
+#         num_str = "|{0:<10d}|{0:^10x}|{0:10b}|".format(i)
+#         num_list.append(num_str)
+#     return num_list
+#
+#
+# print(formatted_numbers())
+
+#
+# import re
+#
+#
+# def find_word(text, word):
+#     if word in text:
+#         search = re.search(word, text)
+#         result1 = {'result': True, 'first_index': search.start(), 'last_index': search.end(),
+#                        'search_string': word, 'string': text}
+#         return result1
+#     elif word not in text:
+#         result2 = {'result': False, 'first_index': None, 'last_index': None,
+#                         'search_string': word, 'string': text}
+#         return result2
+#
+#
+# print(find_word("Guido van Rossum began working on Python in the late 1980s,"
+#                 " as a successor to the ABC programming language, and first"
+#                 " released it in 1991 as Python 0.9.0.", "Python"))
+#
+
+# import re
+#
+#
+# def find_all_words(text, word):
+#     if word in text:
+#         result = re.findall(word, text, re.IGNORECASE)
+#         return result
+#
+#
+# import re
+#
+#
+# def replace_spam_words(text, spam_words):
+#     for i in spam_words:
+#         star = "*" * len(i)
+#         result = re.sub(i, repl=star, string=text, flags=re.IGNORECASE)
+#         text = result
+#     return result
+#
+#
+# print(replace_spam_words("Guido van Rossum began working on Python in the late 1980s,\n"
+#                 "as a successor to the ABC programming language,\n"
+#                 "and first released it in 1991 as Python 0.9.0.", ["began", "Python"]))
+#
+#
+# import re
+#
+#
+# def find_all_emails(text):
+#     #result = re.findall(r"[A-z]+[a-zA-Z0-9._]+@[a-z]+.[a-z]\w{2,}", text)
+#     result = re.findall(r"[a-zA-z][a-zA-Z0-9_.]+@[a-z]+\.\w{2,4}", text)
+#     print(result)
+#     return result
+#
+#
+# find_all_emails('ma@Foolrg Ima.Fool@iana.o 1Fool@iana.org'
+#                 'first_last@iana.org first.middle.last@iana.or A_a@test.com abc111@test.com.net')
+#
+#
+# import re
+#
+#
+# def find_all_phones(text):
+#     result = re.findall(r"\+380?\(?\d{2}\)?\d{3}[\s\-]\d{2}[\s\-]\d{2}|"
+#                         r"\+380?\(?\d{2}\)?\d{3}[\s\-]\d{1}[\s\-]\d{3}", text)
+#     print(result)
+#     return result
+#
+#
+# find_all_phones('Irma +380(67)777-7-771 second +380(67)777-77-77'
+#                  ' aloha a@test.com abc111@test.com.net +380(67)111-777-777+380(67)777-77-787')
+#
+# import re
+#
+#
+# def find_all_links(text):
+#     result = []
+#     iterator = re.finditer(r"https?:\/\/\w*\.?(\w+\.)+\w{2,5}", text)
+#     for match in iterator:
+#         result.append(match.group())
+#     print(result)
+#     return result
+#
+#
+# find_all_links('The main search site in the world is https://www.google.com'
+#                ' The main social network for people in the world is https://www.facebook.com'
+#                ' But programmers have their own social network http://github.com'
+#                ' There they share their code. some url to check https://www..facebook.com www.facebook.com')

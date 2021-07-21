2f8b702e70c78c98587e166e73d5408f4f70334b
Ivan Grigorev <iv.st.grigorev@gmail.com>
2021-07-21 13:50:45 +0300

    Module_1

---
+++ Block_1-Python_Core/Module_1/lesson_2-2.py
@@ -0,0 +1,27 @@
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+#
+# x = - b / a
+# print(f" x = {round(x, 2)}")
+
+# s = (a * b) / 2
+# c = (a ** 2 + b ** 2)
+
+# from math import sqrt
+#
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+#
+# s = (a * b) / 2  # площадь прямоугольника треугольника
+#
+# c_classic = (a ** 2 + b ** 2) ** (1 / 2)
+# c_library_method = sqrt(a ** 2 + b ** 2)
+#
+# print("Square = ", s)
+# print("Hipotenuza = {}".format(c_classic))
+
+# first_name = "Ivan"
+# last_name = "Grigorev"
+# full_name = first_name + " " + last_name
+# message = f"Уважаемый {first_name}, мы сообщаем вам,\nчто вы приобрели билет для путешествия на остров Маврикий.\nОтправление 31 июня сего года.\nПросьба обратится за билетом в кассу аэропорта.\nПри себе иметь паспорт на {full_name}.\nС нетерпением ожидаем вас."
+# print(message)
---
+++ Block_1-Python_Core/Module_1/lesson_3-1.py
@@ -0,0 +1,46 @@
+# Вывести большее из 2 чисел
+# a = 1
+# b = 2
+#
+# if a > b:
+#     print(a)
+# elif b > a:
+#     print(b)
+#
+# # Вывести числа от 1 до 10
+# a = 0
+#
+# for a in range(11, 20, 2):
+#     print(a, end=" ")
+
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# print("a + b =", a + b)
+# print("a - b =", a - b)
+# print("a * b =", a * b)
+# print("a // b =", a // b)
+
+# price = 15
+# money = int(input("1:"))
+#
+# if money < price:
+#     print("You haven`t money")
+# elif money > price:
+#     print("You can buy!")
+#
+
+a = 0
+for i in range(1, 4):
+    a += i
+print(a)
+
+# calc = input("Let`s calculate.")
+#
+# while calс != 0:
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# print("a + b =", a + b)
+# print("a - b =", a - b)
+# print("a * b =", a * b)
+# print("a // b =", a // b)
+

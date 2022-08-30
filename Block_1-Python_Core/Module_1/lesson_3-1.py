7d1b922143664d39023ea5cb7d2332ff83fa93c6
Ivan Grigorev <82472551+Ivan-Grigorev@users.noreply.github.com>
2022-08-30 16:39:49 +0200

    Update lesson_3-1.py

--- Block_1-Python_Core/Module_1/lesson_3-1.py
+++ Block_1-Python_Core/Module_1/lesson_3-1.py
@@ -1,46 +1,48 @@
-# Вывести большее из 2 чисел
-# a = 1
-# b = 2
-#
-# if a > b:
-#     print(a)
-# elif b > a:
-#     print(b)
-#
-# # Вывести числа от 1 до 10
-# a = 0
-#
-# for a in range(11, 20, 2):
-#     print(a, end=" ")
+# Display max number in 2 numbers
+a = 1
+b = 2
 
-# a = int(input("Enter a: "))
-# b = int(input("Enter b: "))
-# print("a + b =", a + b)
-# print("a - b =", a - b)
-# print("a * b =", a * b)
-# print("a // b =", a // b)
+if a > b:
+    print(a)
+elif b > a:
+    print(b)
 
-# price = 15
-# money = int(input("1:"))
-#
-# if money < price:
-#     print("You haven`t money")
-# elif money > price:
-#     print("You can buy!")
-#
+# Dispaly numbers from 1 to 10
+a = 0
+
+for a in range(11, 20, 2):
+    print(a, end=" ")
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a // b =", a // b)
+
+
+price = 15
+money = int(input("1:"))
+
+if money < price:
+    print("You haven`t money")
+elif money > price:
+    print("You can buy!")
+
 
 a = 0
 for i in range(1, 4):
     a += i
 print(a)
 
-# calc = input("Let`s calculate.")
-#
-# while calс != 0:
-# a = int(input("Enter a: "))
-# b = int(input("Enter b: "))
-# print("a + b =", a + b)
-# print("a - b =", a - b)
-# print("a * b =", a * b)
-# print("a // b =", a // b)
 
+calculator = input("Let`s calculate.")
+
+while calсulator != 0:
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+
+    print("a + b =", a + b)
+    print("a - b =", a - b)
+    print("a * b =", a * b)
+    print("a // b =", a // b)

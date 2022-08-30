8db17e9d251ac71a3095699f62c6954faf72a35a
Ivan Grigorev <82472551+Ivan-Grigorev@users.noreply.github.com>
2022-08-30 16:32:29 +0200

    Update lesson_2-2.py

--- Block_1-Python_Core/Module_1/lesson_2-2.py
+++ Block_1-Python_Core/Module_1/lesson_2-2.py
@@ -1,27 +1,30 @@
-# a = int(input("Enter a: "))
-# b = int(input("Enter b: "))
-#
-# x = - b / a
-# print(f" x = {round(x, 2)}")
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
 
-# s = (a * b) / 2
-# c = (a ** 2 + b ** 2)
+x = - b / a
+print(f" x = {round(x, 2)}")
 
-# from math import sqrt
-#
-# a = float(input("Enter a: "))
-# b = float(input("Enter b: "))
-#
-# s = (a * b) / 2  # площадь прямоугольника треугольника
-#
-# c_classic = (a ** 2 + b ** 2) ** (1 / 2)
-# c_library_method = sqrt(a ** 2 + b ** 2)
-#
-# print("Square = ", s)
-# print("Hipotenuza = {}".format(c_classic))
+s = (a * b) / 2
+c = (a ** 2 + b ** 2)
 
-# first_name = "Ivan"
-# last_name = "Grigorev"
-# full_name = first_name + " " + last_name
-# message = f"Уважаемый {first_name}, мы сообщаем вам,\nчто вы приобрели билет для путешествия на остров Маврикий.\nОтправление 31 июня сего года.\nПросьба обратится за билетом в кассу аэропорта.\nПри себе иметь паспорт на {full_name}.\nС нетерпением ожидаем вас."
-# print(message)
+
+from math import sqrt
+
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+
+s = (a * b) / 2  # area of triangle rectangle
+
+c_classic = (a ** 2 + b ** 2) ** (1 / 2)
+c_library_method = sqrt(a ** 2 + b ** 2)
+
+print("Square = ", s)
+print("Hipotenuza = {}".format(c_classic))
+
+
+first_name = "Ivan"
+last_name = "Grigorev"
+full_name = first_name + " " + last_name
+message = f"Dear {first_name}, we inform you\nthat you have purchased a ticket for a trip to the island of Mauritius.\nDeparture on June 31 this year.\nPlease apply for a ticket at the airport ticket office.\nPlease bring your passport for {full_name}.\nС looking forward to seeing you."
+print(message)

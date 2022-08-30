79722a26b9e797c804347b3439f118ca9890d224
Ivan Grigorev <82472551+Ivan-Grigorev@users.noreply.github.com>
2022-08-30 17:01:40 +0200

    Update lesson_m2-2.py

--- Block_1-Python_Core/Module_2/lesson_m2-2.py
+++ Block_1-Python_Core/Module_2/lesson_m2-2.py
@@ -1,38 +1,35 @@
-# n = int(input(": "))
-# n = abs(n)
-# ans = 0
-# while n:
-#     ans += n % 10
-#     n //= 10
-# print(ans)
+n = int(input(": "))
+n = abs(n)
+ans = 0
+while n:
+    ans += n % 10
+    n //= 10
+print(ans)
 
-# login = "admin"
-# password = "root"
-# ans = 1
-#
-# while ans:
-#
-#
-# text = "kmv,nz,vn ,z vc,asfb,an c,zn"
-# s = "k"
-# r = 0
-#
-# for i in text:
-#     if i in "k":
-#         r += 1
-#     print(r)
 
-# n = 5
-# ans = 1
-# for i in range(1, n + 1):О
-#     ans *= i
-# print(ans)
 
-# n = 5
-# ans = 1
-# while n:
-#     ans *= n
-#     n -= 1
-# print(ans)
-#
-#
+text = "kmv,nz,vn ,z vc,asfb,an c,zn"
+s = "k"
+r = 0
+
+for i in text:
+    if i in "k":
+        r += 1
+    print(r)
+
+
+    
+n = 5
+ans = 1
+for i in range(1, n + 1):О
+    ans *= i
+print(ans)
+
+
+
+n = 5
+ans = 1
+while n:
+    ans *= n
+    n -= 1
+print(ans)
